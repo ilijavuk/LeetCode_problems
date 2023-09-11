ac14f777404a7d9596952e833c583c35601bed25
Ilija Vuk <ilijavuk14@gmail.com>
2023-09-11 08:45:04 +0200

    Product of Array Except Self

---
+++ Arrays and Hashing/ProductOfArrayExceptSelf.ts
@@ -0,0 +1,80 @@
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+
+
+ */
+
+/* First Version - 91ms (67.48%) | 56.23MB (26.34%)*/
+function productExceptSelf(nums: number[]): number[] {
+    const prefix = [];
+    const suffix = []
+    const results = [];
+
+    for(let i = nums.length-1; i >= 0; i--) {
+        const nextSuffix = getItemAtIndex(suffix, i+1);
+        suffix[i] = nextSuffix * nums[i];
+    }
+
+    console.log(prefix, suffix);
+
+    for(let i = 0; i < nums.length; i++) {
+        const nextSuffix = getItemAtIndex(suffix, i+1);
+        const prevPrefix = getItemAtIndex(prefix, i-1);
+
+        prefix[i] =  prevPrefix * nums[i];
+
+        results[i] = prevPrefix * nextSuffix;
+    }
+
+    return results;
+};
+
+function getItemAtIndex(nums: number[], index: number) : number {
+    if(index < 0 || index >= nums.length) return 1;
+
+    return nums[index];
+}
+
+
+
+/* Second Version - 85ms (83.69%) | 54.47MB (62.62%) */
+function productExceptSelf(nums: number[]): number[] {
+    const results = new Array(nums.length).fill(1);
+
+    for(let i = 1; i < nums.length; i++) {
+        results[i] = results[i-1]*nums[i-1];
+    }
+
+    let postfix = 1;
+    for(let i = nums.length-1; i >= 0; i--) {
+        results[i] = results[i] * postfix;
+        postfix *= nums[i];
+    }
+
+    return results;
+};
